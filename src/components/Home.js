8892887571d4f11f50c68b2146491f2b33c5914c
Hasnain <mrajhasnain101@gmail.com>
2023-03-22 23:18:34 +0600

    Home.js was created and config files imported

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+//configaration
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config"
+
+//image
+import NoImage from "../images/no_image.jpg"
+
+const Home = () => {
+    const [ state, setState ]=useState();
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
