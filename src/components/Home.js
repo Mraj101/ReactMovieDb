fbdbae7163bd8035f92cdf1d14834bdcb85001c5
Hasnain <mrajhasnain101@gmail.com>
2023-03-28 21:30:15 +0600

    added api key but not working

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,17 +12,16 @@ const Home = () => {
     const [loading,setLoading]=useState(false);
     const [error,setError]=useState(false);
 
-    const fetchMovies=async (page,searchTerm="")=>{
+    const fetchMovies=async (page,searchTerm = '')=>{
       try{
         setError(false);
         setLoading(true);
-        const movies=await API.fetchMovies(page,searchTerm);
+        const movies=await API.fetchMovies(searchTerm,page);
         console.log(movies)
 
       }
       catch(error){
         setError(true)
-        console.log(error.messege)
       }
     };
 
