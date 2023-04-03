12d8ee00a89a4f8b71ee36fd0a9f46acff37cec6
Hasnain <mrajhasnain101@gmail.com>
2023-04-03 19:24:01 +0600

    home were checked and styles for hero image but there is an issue

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-export const Wrapper=styled.div``;
+
+export const Wrapper=styled.div`
+    background: linear-gradient(
+        to bottom,
+        rgba(0,0,0,0)41%,
+        rgba(0,0,0,0.65) 
+        100%
+    ),
+    url(${({image})=> image }),var(--darkgrey);
+    background-size:100%,cover;
+    background-position:center;
+    height:600px;
+    position: relative;
+`;
 export const Content=styled.div``;
 export const Text=styled.div``;
 export const Heading=styled.div``;
