296931a88083d167c06e2a510132b3686d5db47a
Hasnain <mrajhasnain101@gmail.com>
2023-04-11 03:11:44 +0600

    more commit

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -2,16 +2,11 @@ import styled from "styled-components";
 
 
 export const Wrapper=styled.div`
-    background: linear-gradient(
-        to bottom,
-        rgba(0,0,0,0)41%,
-        rgba(0,0,0,0.65) 
-        100%
-    ),
-    url(${({image})=> image }),var(--darkgrey);
+    height:300px;
+    border: 2px solid red;
+    background:url(${({image})=> image });
     background-size:100%,cover;
     background-position:center;
-    height:600px;
     position: relative;
 `;
 export const Content=styled.div``;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
