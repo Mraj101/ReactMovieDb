dd8c8c95fb41f71b750980bd2ee3dcef1ce1551f
Hasnain <mrajhasnain101@gmail.com>
2023-04-12 22:56:47 +0600

    styles tweaked

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -9,8 +9,8 @@ export const GlobalStyle=createGlobalStyle`
         --white:#fff;
         --medGrey:#eee;
         --darkGrey:#1c1c1c;
-        --headerHeight: 15vh;
-        --footerHeight: 15vh;
+        --headerHeight: 12vh;
+        --footerHeight: 12vh;
     }
 
     *{
